dfc4c9b02e18db7208e3e2aae1e37f38376ad8d4
MRezaSaedi <mreza.saedi@gmail.com>
2022-06-02 22:16:43 -0700

    2nd commit modification

---
+++ src/com/syntax/CodingTasks/Array04.java
@@ -0,0 +1,11 @@
+package com.syntax.CodingTasks;
+
+public class Array04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HBD");
+
+	}
+
+}
