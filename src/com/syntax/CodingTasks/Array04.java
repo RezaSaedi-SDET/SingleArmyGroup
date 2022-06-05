416623278e179a5999b68afe1b0618f38840cfbc
MRezaSaedi <mreza.saedi@gmail.com>
2022-06-04 22:29:23 -0700

    06-04-2022

--- src/com/syntax/CodingTasks/Array04.java
+++ src/com/syntax/CodingTasks/Array04.java
@@ -3,9 +3,22 @@ package com.syntax.CodingTasks;
 public class Array04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HBD");
-
-	}
-
+		
+	int treesize=0;
+	int treesize2=0;
+		for(int year=1;year<=3;year++) { 
+			treesize++;
+			System.out.println("year "+year+" - "+"growth "+"1"+" cm");
+			System.out.println("tree size: "+treesize);
+			
+		} 
+		for (int year=4;year<10;year++) {
+			treesize2+=2;
+			System.out.println("year "+year+" - "+"growth "+"2"+" cm");
+			System.out.println("tree size: "+treesize2);
+			
+		}
+			}
 }
+
+		
---
+++ src/com/syntax/CodingTasks/ReplitLoopHW.java
@@ -0,0 +1,106 @@
+package com.syntax.CodingTasks;
+
+public class ReplitLoopHW {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Write a program to print out the pattern:
+		 * 
+		 * Expected output:
+		 * 
+		 * $$$$ $ $ $ $ $$$$
+		 */
+	/*
+	 * 	String usd="$";
+		int i;
+		for(i=1;i<=4;i++) {
+			usd.equals(i);
+			System.out.print(usd);
+		}
+		System.out.println(" ");
+		
+		for(i=1;i<=1;i++) {
+			usd.equals(i);
+			System.out.print(usd+"  "+usd);
+		}
+		System.out.println(" ");
+
+		for(i=1;i<=1;i++) {
+			usd.equals(i);
+			System.out.print(usd+"  "+usd);
+		}
+		System.out.println(" ");
+		
+		for(i=1;i<=4;i++) {
+			usd.equals(i);
+			System.out.print(usd);
+		}
+	 */
+
+		
+		/*
+		 * Write a program to print out the pattern:
+		 * 
+		 * 1 2 3 4 5 6 7 8 9 10
+		 * 
+		 * 2 4 6 8 10 12 14 16 18 20
+		 * 
+		 * 3 6 9 12 15 18 21 24 27 30
+		 * 
+		 * 4 8 12 16 20 24 28 32 36 40
+		 * 
+		 * 5 10 15 20 25 30 35 40 45 50
+		 */
+		
+	/*
+	 	 int i;
+
+		for (i = 1; i <= 10; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println(" ");
+		for (i = 2; i <= 20; i += 2) {
+			System.out.print(i + " ");
+		}
+		System.out.println(" ");
+		for (i = 3; i <= 30; i += 3) {
+			System.out.print(i + " ");
+		}
+		System.out.println(" ");
+		for (i = 4; i <= 40; i += 4) {
+			System.out.print(i + " ");
+		}
+		System.out.println(" ");
+		for (i = 5; i <= 50; i += 5) {
+			System.out.print(i + " ");
+			{
+	 */
+		
+
+		/*
+		 * For you to do:
+		 * 
+		 * Write a program to print out the pattern:
+		 * 
+		 * syntax
+		 * 
+		 * syntax
+		 * 
+		 * syntax
+		 * 
+		 * syntax
+		 * 
+		 * syntax
+		 * 
+		 * syntax
+		 */
+
+//		for (int i = 0; i < 6; i++) {
+//			System.out.println("syntax");
+//			System.out.println(" ");
+
+//		}
+	}
+
+}
